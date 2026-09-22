51e464437061c73b229502dac4146224b1417664
Gatgatt <gaetan.tracqui@epfl.ch>
2026-09-22 23:26:20 +0200

    Refine comments and update rotation calculation
    Updated comments for clarity and adjusted rotation logic to reflect degrees instead of fixed increments.

--- ROS_Software_Assignement/rover_commands/rover_commands/gps.py
+++ ROS_Software_Assignement/rover_commands/rover_commands/gps.py
@@ -25,12 +25,12 @@ class GPSNode(Node):
         rz = msg.angular.z
 
         # Ensure the command is valid
-        # For level 3, we omit the forbidden move check because of the sensor Node.
+        # For level 3, we omit the forbidden move check because of the sensor Node, we also remove the logs for the same reason.
         '''
         if ((tz != 0.0 and ry != 0.0) or (tx != 0.0 and tz != 0.0)):
             self.get_logger().warn('Forbidden move')
             return
-        '''
+        
 
         # Log what the command was interpreted as
         if tx != 0.0 and ry != 0.0:
@@ -41,9 +41,10 @@ class GPSNode(Node):
             self.get_logger().info(f'Slide {"Right" if tz > 0 else "Left"}')
         elif ry != 0.0:
             self.get_logger().info(f'Rotating on itself to the {"Left" if ry > 0 else "Right"}')
+        '''
 
         # Figure out the rotation first, then move in consequence
-        self.position['ry'] = (self.position['ry'] + ry * 90.0) % 360.0   # Rotate by 1 unit = 90° when a rotation command is received
+        self.position['ry'] = (self.position['ry'] + ry) % 360.0   # Rotate by ry degrees when a command is received
         theta = math.radians(self.position['ry'])
         # Update x and z based on the updated rotation of the rover and the linear component of the command
         self.position['x'] += round(tx * math.cos(theta) + tz * math.sin(theta), 6)
